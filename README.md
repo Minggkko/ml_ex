f0dd124172ce2edb76db4eb45ae0e37428fdafb1
Minggkko <gimmyy05@gmail.com>
2025-12-02 14:29:57 +0900

    file path 지정 방법 번경, readme MD 파일 수정

--- README.md
+++ README.md
@@ -5,3 +5,30 @@
 ```
 pip insatll streamlit
 ```
+# streamlit 배포시 필수 사항
+- os는 linux임, 인식하는 path로 변경해야함
+# 1. 파일 path
+- 예시 : 모델 로딩 방법
+``
+@st.cache_resource # 자원 캐싱 기능
+def load_model():
+    model_path = os.path.join(base_path, "models", "iris_model_rfc.pkl") # models/iris_model_rfc.pkl
+    # with open('models/iris_model_rfc.pkl', 'rb') as f:
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+    return model
+model = load_model()
+```
+
+
+
+# requiremnts.txt 파일 생성방법
+```
+pip freeze > requirements.txt
+```
+
+# 예시 requitremts 
+numpy==2.3.5
+streamlit==1.15.0
+scikit-learn==1.7.2
+joblib==1.5.2
--- ml_deploy/app_st.py
+++ ml_deploy/app_st.py
@@ -3,30 +3,48 @@ import numpy as np
 import pickle
 import os
 
+# 현재 파일의 디렉토리 경로 설정
+# ./ : 현재 디렉토리
+base_path = os.path.dirname(__file__)
+print("Base path:", base_path)
+
 # 모델 로드 함수
 @st.cache_resource # 자원 캐싱 기능
 def load_model():
-    base_path = os.path.dirname(__file__)
-    model_path = os.path.join(base_path, "models", "iris_model_lr.rfc.pkl")
+    model_path = os.path.join(base_path, "models", "iris_model_rfc.pkl") # models/iris_model_rfc.pkl
+    # with open('models/iris_model_rfc.pkl', 'rb') as f:
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
     return model
 model = load_model()
 
 # 클래스별 이미지 경로 설정
+# 윈도우에서만 됨
+# def get_image_path(prediction):
+#     if prediction == 0:
+#         return "static/setosa.jpg" # setosa 이미지 경로
+#     elif prediction == 1:
+#         return "static/versicolor.jpg" # versicolor 이미지 경로
+#     else:
+#         return "static/virginica.png" # virginica 이미지 경로
+
+# 모든 OS에서 작동하도록 수정
 def get_image_path(prediction):
     if prediction == 0:
-        return "static/setosa.jpg" # setosa 이미지 경로
+        return os.path.join(base_path, 'static', 'setosa.jpg') # setosa 이미지 경로
     elif prediction == 1:
-        return "static/versicolor.jpg" # versicolor 이미지 경로
+        return os.path.join(base_path, 'static', 'versicolor.jpg') # versicolor 이미지 경로
     else:
-        return "static/virginica.png" # virginica 이미지 경로
-    
-   
-    
+        return os.path.join(base_path, 'static', 'virginica.png') # virginica 이미지 경로
+
+# 메인 실행 코드
 # Streamlit 앱 구성
 st.title("Iris 품종 예측")
 st.write("꽃받침 길이, 너비, 꽃잎 길이, 너비를 입력하여 품종을 예측해보세요.")
+
+# 이미지 로딩
+img_path = "static/flower1.jpg"
+st.image(img_path, caption="꽃")
 
 # 사용자 입력 받기
 sepal_length = st.number_input("꽃받침 길이",
@@ -40,31 +58,33 @@ petal_width = st.number_input("꽃잎 너비",
 
 # 예측하기 버튼
 if st.button("예측하기"):
-    #예측 버튼을 누르기 전하고 눌렀을때
+    # 예측 버튼을 눌렀을 때
     btn_clicked = True
-else : 
+else:
+    # 예측 버튼을 누르지 않았을 때
     btn_clicked = False
-    # 누르지 않았을때
-st.markdown("---")
 
-if btn_clicked :
-           
+st.markdown("---")
+st.subheader("예측 결과 출력")
+
+if btn_clicked == True:
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     # 예측 수행
     prediction = model.predict(input_data)
-    #예측값을 터미널에 출력(디버그용)
-    print(prediction)
     predicted_class = prediction[0]
     # 예측된 클래스 이름
     class_name = ['Setosa', 'Versicolor', 'Virginica'][predicted_class]
-    st.subheader(f"예측된 품종: {class_name}")
+    # # 예측된 품종에 해당하는 이미지 출력
+    # image_path = get_image_path(predicted_class)
+    # st.image(image_path, caption=class_name)
 
-    # 예측된 품종에 해당하는 이미지 출력
-    # 이미지 크기 고정
-    col1, col2, col3 = st.columns([1, 5, 1])
-    with col2:
+    col1, col2, col3 = st.columns([1,5,1])
+
+    with col1:
         st.subheader(f"예측된 품종 : {class_name}")
         image_path = get_image_path(predicted_class)
         st.image(image_path, caption=class_name)
-else :
-    st.warning("값을 입력하고 ")
+
+
+else:
+    st.warning("값을 입력하고, 예측하기 버튼을 눌러주세요.")
